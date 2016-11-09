2bb23402afd3aafbdf05e157c08b4b349dabeed7
fgonzales16 <francisgonzales@francis.local>
2016-11-09 17:32:52 +0800

    fix

--- spec/spec_helper.rb
+++
@@ -1,8 +0,0 @@
-$: << File.expand_path("../../lib", __FILE__)
-require "language"
-
-RSpec::Matchers.define :have_constant do |const|
-  match do |owner|
-    owner.const_defined?(const)
-  end
-end
